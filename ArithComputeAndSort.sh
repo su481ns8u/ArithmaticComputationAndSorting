6ab575da871e4725ab27a2fe28cc99b28ccc6b7b
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-04-30 01:06:52 +0530

    Use Case 6 pushed

--- ArithComputeAndSort.sh
+++ ArithComputeAndSort.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+declare -A ArithOps
+
 echo "Enter three numbers:"
 read -p "a: " a
 read -p "b: " b
@@ -9,3 +11,11 @@ d=$(($a + $b * $c))
 e=$(($a * $b + $c))
 f=$(($c + $a / $b))
 g=$(($a % $b + $c))
+
+ArithOps[o1]=$d
+ArithOps[o2]=$e
+ArithOps[o3]=$f
+ArithOps[o4]=$g
+
+echo ${ArithOps[@]}
+echo ${!ArithOps[@]}
